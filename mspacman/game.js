23842bd5ae0f41684886c7b51b58929d2ad50d02
rlrlarson <rebecca.larson@tufts.edu>
2015-03-02 20:45:32 -0500

    Corrected game.jsso there were no errors using JSLint

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,11 +1,11 @@
+/*jslint browser:true */
 function init() {
-	var canvas = document.getElementById('game_canvas');
-	var ctx = canvas.getContext('2d');
-	
-	var img = new Image();
-	img.src = "pacman10-hp-sprite.png";
-	img.onload = function() {
-		ctx.drawImage(img, 313, 0, 488, 138, 0, 0, 488, 138);
-		ctx.drawImage(img, 83, 23, 20, 18, 222, 63, 20, 18);
-	};
+    "use strict";
+    var canvas = document.getElementById('game_canvas'), ctx = canvas.getContext('2d'), img = new Image();
+
+    img.src = "pacman10-hp-sprite.png";
+    img.onload = function () {
+        ctx.drawImage(img, 313, 0, 488, 138, 0, 0, 488, 138);
+        ctx.drawImage(img, 83, 23, 20, 18, 222, 63, 20, 18);
+    };
 }
