4c54bc849d6fc989d25bdc2377797de5d6dbbf8d
rlrlarson <rebecca.larson@tufts.edu>
2015-03-02 20:14:23 -0500

    Updated game.js to load Google image and Ms. Pacman

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,11 @@
-// Replace this with your code...
+function init() {
+	var canvas = document.getElementById('game_canvas');
+	var ctx = canvas.getContext('2d');
+	
+	var img = new Image();
+	img.src = "pacman10-hp-sprite.png";
+	img.onload = function() {
+		ctx.drawImage(img, 313, 0, 488, 138, 0, 0, 488, 138);
+		ctx.drawImage(img, 83, 23, 20, 18, 222, 63, 20, 18);
+	};
+}
