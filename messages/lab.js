c3eb96e11764c260da970b7f4fa9a0daae228d06
rlrlarson <rlrlarson@users.noreply.github.com>
2015-03-10 00:30:29 -0400

    gave correct files

--- messages/lab.js
+++ messages/lab.js
@@ -3,7 +3,7 @@ function parse(){
 	request = new XMLHttpRequest();
 	
 	// Step 2: create or “open” HTTP request
-	request.open(“GET”, “messages.json”, true);
+	request.open(“GET”, “data.json”, true);
 	
 	// Step 3: set up a way to manage the response —to a function
 	request.onreadystatechange = parseData;
